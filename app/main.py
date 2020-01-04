b3c014c73016ba8a62e326bcdab89064b6624c04
Salam Safi <salamalmajdi@gmail.com>
2020-01-05 09:25:25 +1100

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 import os
-import os.path
-from os import path
-
 
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls')
 
 class Room:
     def __init__(self, id, text, options):
@@ -26,11 +23,16 @@ class Player:
         self.name = name
         self.health = health
 
+class GameData:
+    def __init__(self, room, health):
+        self.room = room
+        self.health = health
+
 def decideUponOption(option):
     if option == "dead":
-        start()
+        printInstructions()
     elif option == "won":
-        start()
+        printInstructions()
 
 rooms = {
     "room-1": Room("room-1", "You are in room 1", options = {
@@ -53,9 +55,7 @@ rooms = {
     })
 }
 
-playing = True
-
-def start():
+def printInstructions():
     cls()
     print("welcome to game test")
     introput = input("Press enter to continue: ")
@@ -63,37 +63,56 @@ def start():
         introput = input()
     cls()
 
-username = input('enter username: ')
-if path.exists(username + ".txt"):
-    f = open(username + ".txt", "r")
-    currentRoom = rooms[f.readline().rstrip()]
-    player = Player(username, f.readline().rstrip())
-else:
-    currentRoom = rooms["room-1"]
-    f = open(username + ".txt", "w")
-    f.write(currentRoom.id)
-    f.write("\n")
-    f.write("100")
-    f.close
-    player = Player(username, "100")
-
-start()
-while playing:
-    printroom(currentRoom)
-    for key in currentRoom.options.values():
-        print(key.text)
-    print(player.name, "Health: " + player.health)
-    input1 = input("Input: ")
-    if input1 == 'q':
-        f = open(username + ".txt", "w")
-        f.write(currentRoom.id)
-        f.write("\n")
-        f.write(player.health)
-        f.close
-        break
-    if currentRoom.options[input1].id in rooms.keys():
-        currentRoom = rooms[currentRoom.options[input1].id]
-    else:
-        decideUponOption(currentRoom.options[input1].id)
-        currentRoom = rooms["room-1"]
+def login():
     cls()
+    return input('enter username: ')
+
+def saveGameData(username, gameData):
+    f = open(username + ".txt", "w")
+    f.write(gameData.room)
+    f.write("\n")
+    f.write(str(gameData.health))
+    f.write("\n")
+    f.close
+
+
+def loadGameData(username):
+    f = open(username + ".txt", "r")
+    room = f.readline().rstrip()
+    health = int(f.readline().rstrip())
+    gameData = GameData(room, health)
+    f.close
+    return gameData
+
+def initGameData(username):
+    if not os.path.exists(username + ".txt"):
+        gameData = GameData("room-1", 100)
+        saveGameData(username, gameData)
+    else:
+        gameData = loadGameData(username)
+    return gameData
+
+def playGame():
+    username = login()
+    gameData = initGameData(username)
+    printInstructions()
+    player = Player(username, gameData.health)
+    currentRoomId = gameData.room
+    currentRoom = rooms[currentRoomId]
+    while True:
+        printroom(currentRoom)
+        for key in currentRoom.options.values():
+            print(key.text)
+        print(player.name, "Health: " + str(player.health))
+        input1 = input("Input: ")
+        if input1 == 'q':
+            saveGameData(username, GameData(currentRoom.id, player.health))
+            cls()
+            break
+        if currentRoom.options[input1].id in rooms.keys():
+            currentRoom = rooms[currentRoom.options[input1].id]
+        else:
+            decideUponOption(currentRoom.options[input1].id)
+        cls()
+
+playGame()
