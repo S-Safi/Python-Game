5b32b13131d4e5b0d66f421297bd1f8effbbfcb9
Salam Safi <salamalmajdi@gmail.com>
2020-01-11 16:00:09 +1100

    updated room system

--- app/main.py
+++ app/main.py
@@ -1,16 +1,39 @@
 import os
 saveDirectory = "saves"
+roomDirectory = "rooms"
 
 def getSaveFilePath(username):
     return saveDirectory + '/' + username + ".txt"
 
+def getRoomFilePath(roomName):
+    return roomDirectory + '/' + roomName + ".txt"
+
 def cls():
     os.system('cls')
+
 class Room:
     def __init__(self, id, text, options):
         self.id = id
         self.text = text
         self.options = options
+
+def readRoom(roomName):
+    f = open(getRoomFilePath(roomName), "r")
+    line = f.readline().rstrip()
+    fullText = ''
+    while line != "---":
+        fullText = fullText + line
+        line = f.readline().rstrip()
+    options = {}
+    i = 1
+    line = f.readline().rstrip()
+    while line != "|-|":
+        Id = f.readline().rstrip()
+        options[str(i)] = Option(line, Id)
+        line = f.readline().rstrip()
+        i += 1
+    room = Room(roomName, fullText, options)
+    return room
 
 def printroom(room):
            print("-----------")
@@ -30,33 +53,6 @@ class GameData:
     def __init__(self, room, health):
         self.room = room
         self.health = health
-
-def decideUponOption(option):
-    if option == "dead":
-        printInstructions()
-    elif option == "won":
-        printInstructions()
-
-rooms = {
-    "room-1": Room("room-1", "You are in room 1", options = {
-        "1": Option("1. Go West", "room-2"),
-        "2": Option("2. Go East", "room-3")
-    }),
-    "room-2": Room("room-2", "You are in room 2", options = {
-        "1": Option("1. Go West", "room-4"),
-        "2": Option("2. Go East", "room-1")
-    }),
-    "room-3": Room("room-3", "You are in room 3", options = {
-        "1": Option("1. Go West", "room-1"),
-        "2": Option("2. Go East", "room-5")
-    }),
-    "room-4": Room("room-4", "You are in room 4", options = {
-        "r": Option("r. you have died, press r to restart", "dead")
-    }),
-    "room-5": Room("room-5", "You are in room 5", options = {
-        "r": Option("r. you win, press r to restart", "won")
-    })
-}
 
 def printInstructions():
     cls()
@@ -106,7 +102,7 @@ def playGame():
     printInstructions()
     player = Player(username, gameData.health)
     currentRoomId = gameData.room
-    currentRoom = rooms[currentRoomId]
+    currentRoom = readRoom(currentRoomId)
     while True:
         printroom(currentRoom)
         for key in currentRoom.options.values():
@@ -117,10 +113,6 @@ def playGame():
             saveGameData(username, GameData(currentRoom.id, player.health))
             cls()
             break
-        if currentRoom.options[input1].id in rooms.keys():
-            currentRoom = rooms[currentRoom.options[input1].id]
-        else:
-            decideUponOption(currentRoom.options[input1].id)
+        currentRoom = readRoom(currentRoom.options[input1].id)
         cls()
-
 playGame()
